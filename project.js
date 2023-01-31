b21e76a513c00ade636196d365ee91b168b8c1dd
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2023-01-31 01:45:58 +0100

    Mobile design applied

--- project.js
+++ project.js
@@ -9,6 +9,7 @@ const incomeValue = document.querySelector("#incomeValue"); //wartość przychod
 const expenseValue = document.querySelector("#expenseValue"); //wartość wydatku
 const incomesSumSpan = document.querySelector("#incomes_value"); //span do "wyplucia sumy dochodów"
 const expensesSumSpan = document.querySelector("#expenses_value"); //span do "wyplucia sumy wydatków"
+const balanceInfo = document.querySelector("#balanceInfo"); //Informacja o stanie konta
 
 const incomes = []; //W tej tablicy będą zapisywane dochody
 const expenses = []; //W tej tablicy będą zapisywane wydatki
@@ -29,14 +30,13 @@ function addIncome(income) {
   span.innerText = income.amount;
 
   const btnContainer = document.createElement("div");
-  btnContainer.classList = "buttons_container"; //Ta klasa jest w CSS
 
   const editBtn = document.createElement("button");
-  editBtn.classList = "list_item_button edit_button"; //Te klasy są w CSS...
+  editBtn.classList = "list_item_button edit_button";
   editBtn.innerText = "Edytuj";
 
   const removeBtn = document.createElement("button");
-  removeBtn.classList = "list_item_button delete_button"; //...I te również :-)
+  removeBtn.classList = "list_item_button delete_button";
   removeBtn.innerText = "Usuń";
 
   btnContainer.appendChild(editBtn);
@@ -48,12 +48,15 @@ function addIncome(income) {
 
   incomesList.appendChild(li);
 
-  editBtn.addEventListener("click", () => {
+  incomeTitle.value = "";
+  incomeValue.value = "";
+  //funkcja do edytowania elementu listy incomesList
+  editBtn.addEventListener("click", function () {
     editItem(income);
   });
-
-  removeBtn.addEventListener("click", () => {
-    incomesList.removeChild(li);
+  //funkcja do usuwania elementu listy incomesList
+  removeBtn.addEventListener("click", function () {
+    deleteItem();
   });
 }
 
@@ -61,30 +64,25 @@ function editItem(income) {
   console.log(income);
 }
 
-// function deleteItem(incomes) {
-//   console.log(incomes);
-// }
+function deleteItem() {
+  incomes.filter((income) => {
+    incomes.splice(income, 1);
+  });
+
+  renderIncomesList();
+  sumIncomes();
+}
 
 // function deleteItem(income) {
 //   console.log(income);
-
 //   incomes.filter((item) => {
 //     return income.id !== item.id;
 //   });
-
 //   renderIncomesList();
 //   sumIncomes();
 // }
 
-//funkcja renderująca listę (wyświetlająca listę w body)
-function renderIncomesList() {
-  incomesList.innerHTML = "";
-  incomes.forEach((income) => {
-    addIncome(income);
-  });
-  incomeTitle.value = "";
-  incomeValue.value = "";
-}
+//funkcja sumująca wszystkie income.amount z incomes i dodająca je do zmiennej sumOfIncomes
 
 function sumIncomes() {
   let sumOfIncomes = 0; // zmienna pomocnicza - przechowuje sumę przychodów
@@ -92,6 +90,13 @@ function sumIncomes() {
     sumOfIncomes += income.amount;
   });
   incomesSumSpan.innerText = sumOfIncomes;
+}
+//funkcja renderująca listę (wyświetlająca listę w body)
+function renderIncomesList() {
+  incomesList.innerHTML = "";
+  incomes.forEach((income) => {
+    addIncome(income);
+  });
 }
 
 function addNewIncome() {
@@ -107,15 +112,15 @@ function addNewIncome() {
     if (!newIncome.title) {
       alert("Podaj tytuł Twojego przychodu!");
     } else {
-      incomes.push(newIncome);
+      incomes.push(newIncome); //Dodanie obiektu do tablicy
     }
   } else {
     alert("Podaj liczbę większą od zera!");
   }
   //Teraz newIncome (jako obiekt) jest już w tablicy incomes
-  //Za każdym razem, gdy zostanie dodany nowy income do Array, należy zsumować wyrenderować listę, a następnie zsumować wartości.
-  renderIncomesList();
+  //Za każdym razem, gdy zostanie dodany nowy przychód do Array, należy wyrenderować listę, a następnie zsumować wartości.
   sumIncomes();
+  renderIncomesList();
 }
 
 incomesForm.addEventListener("submit", (e) => {
@@ -138,7 +143,6 @@ function addExpense(expense) {
   span.innerText = expense.amount;
 
   const btnContainer = document.createElement("div");
-  btnContainer.className = "buttons_container";
 
   const editBtn = document.createElement("button");
   editBtn.classList = "list_item_button edit_button";
@@ -161,46 +165,43 @@ function addExpense(expense) {
     editItem(expense);
   });
 
-  removeBtn.addEventListener("click", () => {
-    removeItem(expense);
+  removeBtn.addEventListener("click", function () {
+    removeItem();
   });
+}
+
+function removeItem() {
+  expenses.filter((expense) => {
+    expenses.splice(expense, 1);
+  });
+
+  renderExpensesList();
+  sumExpenses();
 }
 
 // const editItem = (expense) => {
 //   console.log(expense);
 // }
 
-// function removeItem(expense) {
-//   console.log(expense);
-//   expenses.forEach((item) => console.log(item.id === expense.id));
-// }
-
-function removeItem(expense) {
-  console.log(expense);
-  const deleted = expenses.filter((item) => {
-    return item.id !== expense.id;
-  });
-  console.log(deleted);
-}
-
-const renderExpensesList = () => {
+function renderExpensesList() {
   expensesList.innerHTML = "";
   expenses.forEach((expense) => {
     addExpense(expense);
   });
   expenseTitle.value = "";
   expenseValue.value = "";
-};
+}
 
-const sumExpenses = () => {
+function sumExpenses() {
   let sumOfExpenses = 0;
   expenses.forEach((expense) => {
     sumOfExpenses += expense.amount;
   });
-  expensesSumSpan.innerHTML = sumOfExpenses;
-};
+  expensesSumSpan.innerText = sumOfExpenses;
+  console.log(sumOfExpenses);
+}
 
-const addNewExpense = () => {
+function addNewExpense() {
   const newExpense = {
     title: expenseTitle.value,
     amount: Number(expenseValue.value),
@@ -222,7 +223,7 @@ const addNewExpense = () => {
   //Za każdym razem, gdy zostanie dodany nowy expense, należy wyrenderować listę, a następnie zsumować wartości
   renderExpensesList();
   sumExpenses();
-};
+}
 
 expensesForm.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -231,11 +232,16 @@ expensesForm.addEventListener("submit", (event) => {
 
 /* SPRAWDZENIE CO WIĘKSZE */
 // let IncomesSumSpanAsNumber = Number(incomesSumSpan.innerText);
+// console.log(sumOfIncomes);
 // let ExpensesSumSpanAsNumber = Number(expensesSumSpan.innerText);
+// console.log(ExpensesSumSpanAsNumber);
 
-// if(IncomesSumSpanAsNumber > ExpensesSumSpanAsNumber) {
-//   balanceInfo.innerText = `Możesz jeszcze wydać ${IncomesSumSpanAsNumber-ExpensesSumSpanAsNumber} złotych`;
-// } else if(IncomesSumSpanAsNumber < ExpensesSumSpanAsNumber) {
+// if (IncomesSumSpanAsNumber > ExpensesSumSpanAsNumber) {
+//   balanceInfo.innerText = `Możesz jeszcze wydać ${
+//     IncomesSumSpanAsNumber - ExpensesSumSpanAsNumber
+//   } złotych`;
+// }
+// else if(IncomesSumSpanAsNumber < ExpensesSumSpanAsNumber) {
 //   balanceInfo.innerText = `Bilans jest ujemny. Jesteś na minusie ${IncomesSumSpanAsNumber-ExpensesSumSpanAsNumber} złotych`;
 // }
 // else {
